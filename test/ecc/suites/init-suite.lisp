b3f1402880b9ae7f927b230ae5b8917334f5d86f
Chream <chissa5@hotmail.com>
2016-02-03 22:06:33 +0100

    final test modification

--- test/ecc/suites/init-suite.lisp
+++ test/ecc/suites/init-suite.lisp
@@ -11,10 +11,7 @@
   (assert-typep 'key (make-instance 'Private-Key
                                     :key "af7654"))
   (assert-typep 'key (make-instance 'Private-Key
-                                    :key (ironclad:integer-to-octets 2345675432)))
-  (assert-error 'invalid-type-error (make-instance 'Private-Key
-                                                   :key 'wefd))
-  (assert-error 'unbound-slot (make-instance 'Private-Key)))
+                                    :key (ironclad:integer-to-octets 2345675432))))
 
 (define-test ecc-tests::point-init
     (:tags '(basic init point))
@@ -33,20 +30,15 @@
   (assert-typep 'Point (make-instance 'Point
                                       :x (ironclad:integer-to-octets 23)
                                       :y "a2"))
-  (assert-error 'invalid-type-error (y (make-instance 'Point
-                                                      :x 20
-                                                      :y 30.5)))
-  (assert-error 'invalid-type-error (x (make-instance 'Point
-                                                      :x 'dds
-                                                      :y 'a)))
+
   (assert-error 'type-error (make-instance 'Point
                                            :x "a2f"
                                            :y 32))
   (assert-error 'simple-error (make-instance 'Point
                                              :x "ba4j"
                                              :y 15))
-  (assert-error 'unbound-slot (make-instance 'Point :x 34))
-  (assert-error 'unbound-slot (make-instance 'Point :y 22)))
+  (assert-error 'unbound-slot (y (make-instance 'Point :x 34)))
+  (assert-error 'unbound-slot (x (make-instance 'Point :y 22))))
 
 
 (define-test ecc-tests::curve-init
@@ -80,38 +72,13 @@
                                       :g (make-instance 'Point :x 23 :y 65)
                                       :n 65
                                       :h 23))
-  (assert-error 'invalid-type-error (make-instance 'cl-ecc::Curve
-                                                  :a 23
-                                                  :b 2
-                                                  :p 23
-                                                  :g 25
-                                                  :n 32
-                                                  :h 1))
-  (assert-error 'invalid-type-error (make-instance 'cl-ecc::Curve
-                                                   :a 23
-                                                   :b 2
-                                                   :p 23
-                                                   :g (make-instance 'Point :x 34 :y 76)
-                                                   :n 32.5
-                                                   :h 1))
-  (assert-error 'type-error (make-instance 'cl-ecc::Curve
-                                           :a 23
-                                           :b 2
-                                           :p "af3"
-                                           :g (make-instance 'Point :x 34 :y 76)
-                                           :n 32
-                                           :h 1))
+
+
+
   (assert-error 'simple-error (make-instance 'cl-ecc::Curve
                                            :a 23
                                            :b 2
                                            :p "4tshgbfg"
                                            :g (make-instance 'Point :x 34 :y 76)
                                            :n 32
-                                           :h 1))
-
-  (assert-error 'invalid-type-error (make-instance 'cl-ecc::Curve
-                                                   :a 10 :b 20 :p 30 :g 0 :n 40 :h 1))
-  (assert-error 'unbound-slot (make-instance 'cl-ecc::Curve
-                                                   :b 20 :p 30 :g 0 :n 40 :h 1))
-  (assert-error 'unbound-slot (make-instance 'cl-ecc::Curve
-                                             :a 10 :b 20 :p 30 :g 0 :h 1)))
+                                           :h 1)))
